607ac283919df4c6da2c16ef71f21af94cf63b57
James E. Marca <james@activimetrics.com>
2014-11-13 17:17:47 -0800

    fixing test.  abandoned yet again.

--- test/test_put_view.js
+++ test/test_put_view.js
@@ -12,21 +12,51 @@ var cpass = env.COUCHDB_PASS ;
 var chost = env.COUCHDB_HOST || 'localhost';
 var cport = env.COUCHDB_PORT || 5984;
 
-var test_db = env.COUCHDB_TESTDB
+var test_db = env.COUCHDB_TESTDB || 'test%2fput%2fview%2fcode'
 if(!test_db){ throw new Error('need valide db defined in environment variable COUCHDB_TESTDB')}
 var couch = 'http://'+chost+':'+cport+'/'+test_db
 console.log('testing couchdb='+couch)
 
-/**
- * fixme, I should really create a test db, and populate it with data
- * and with a view
- *
- * Instead, because I want to get some real work done, this test is
- * going to just use an exsiting db that I have with existing views
- *
- * If your name is not James E. Marca, you should change this
- *
- */
+var created_locally=false
+before(function(done){
+    // create a test db, the put data into it
+    superagent.put(couch)
+    .type('json')
+    .auth(cuser,cpass)
+    .end(function(e,r){
+        r.should.have.property('error',false)
+        if(!e)
+            created_locally=true
+        // now populate that db with some docs
+
+        var docs = {'docs':[{'_id':'doc1'
+                    ,foo:'bar'}
+                   ,{'_id':'doc2'
+                    ,'baz':'bat'}
+                   ]}
+        _.each([1,2,3,4,5,6,7,8,9],function(v){
+            docs.docs.push({'superb':'timing',
+                            'result':v
+                           })
+            return null
+        })
+        superagent.post(couch+'/_bulk_docs')
+        .type('json')
+        .set('accept','application/json')
+        .send(docs)
+        .end(function(e,r){
+            if(e) done(e)
+            _.each(r.body
+                  ,function(resp){
+                       resp.should.have.property('ok')
+                       resp.should.have.property('id')
+                       resp.should.have.property('rev')
+                   });
+            return done()
+        })
+        return null
+    })
+})
 
 
 var views = ['_design/imputedchecks/_view/missing_wim_neighbors'
